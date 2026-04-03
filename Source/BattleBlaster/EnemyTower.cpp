b094dba525dfd2e0ba8ae4a154c9804fcb0de3f3
Swar00p10 <swaroopassassin@gmail.com>
2026-04-03 22:27:43 +0530

    [Static mesh] Added new Enemy tower class
    Created new class called Enemy tower.
    Added the static meshes.
    Placed 4 of them in the level.

---
+++ Source/BattleBlaster/EnemyTower.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyTower.h"
+
+AEnemyTower::AEnemyTower()
+{
+}
+
+void AEnemyTower::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AEnemyTower::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
---
+++ Source/BattleBlaster/EnemyTower.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseTank.h"
+#include "PlayerTank.h"
+#include "EnemyTower.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLEBLASTER_API AEnemyTower : public ABaseTank
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AEnemyTower();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	UPROPERTY(EditAnywhere)
+	APlayerTank* PlayerTank;
+
+};
